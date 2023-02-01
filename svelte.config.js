845f2b62b545ed2bef15d9b5d31c7347012d3384
JoshBoehm <josh@joshboehm.com>
2023-02-01 12:29:13 -0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,12 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: vercel()
-	}
+    		paths: {
+      		base: main ? '' : '/JoshBoehm/splitio',
+   	},
+    	// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+    	// For example, instead of '_app', use 'app_', 'internal', etc.
+    	appDir: 'internal',
 };
 
 export default config;
